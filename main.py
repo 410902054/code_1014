37db033d4264ab2105ec6e2cd20b2e1d99af6670
410902054 <71751283+410902054@users.noreply.github.com>
2020-10-14 15:36:51 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+def on_forever():
+    for index in range(10):
+        basic.show_number(index)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            if (Y == 1) {
+                continue;
+            }
+            led.plot(X, Y)
+            basic.pause(100)
+        }
+    }
+})
